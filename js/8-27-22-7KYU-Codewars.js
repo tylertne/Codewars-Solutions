52035e277d34a1f29101486dc956bafc91beafce
Tyler Neuschatz <tylertne@gmail.com>
2022-08-28 00:07:37 -0700

    added solution

---
+++ js/8-27-22-7KYU-Codewars.js
@@ -0,0 +1,14 @@
+//Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+function getDivisorsCnt(n){
+    let count = 0;
+   
+   for(let i = 1; i <= 500000; i++) {
+     if(n % i === 0) {
+       count ++
+     }
+   }
+   return count
+ }
