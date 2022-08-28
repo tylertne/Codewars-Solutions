cc5f0c59b7c2a6943d895c00719244496ffe3d92
Tyler Neuschatz <tylertne@gmail.com>
2022-08-28 15:11:46 -0700

    added solution

---
+++ js/8-28-22-7KYU-Codewars.js
@@ -0,0 +1,10 @@
+//Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+function solution(nums){
+    if(nums === [] || nums == null) {
+      return []
+    }
+      else {
+        return nums.sort((a,b) => a - b)
+      }
+    }
