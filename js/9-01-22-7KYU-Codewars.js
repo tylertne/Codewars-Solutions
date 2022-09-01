9bbf2b9e4adfa94c61bbfc045e9ae55f41186d99
Tyler Neuschatz <tylertne@gmail.com>
2022-09-01 14:08:18 -0700

    added solution

--- js/9-01-22-7KYU-Codewars.js
+++ js/9-01-22-7KYU-Codewars.js
@@ -1,4 +1,4 @@
-// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
 
 function sumDigits(number) {
    return Math.abs(number).toString().split('').map(num => Number(num)).reduce((a,b) => a + b)
